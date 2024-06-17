2bd5abe3451ba58561ef084e5e1814360da04b5b
SLASH-7 <basnetujjwal98@gmail.com>
2024-06-18 00:19:21 +0545

    Feat(db): add database,add prisma schema,add theme toggle

--- src/app/(root)/(routes)/page.tsx
+++ src/app/(root)/(routes)/page.tsx
@@ -1,9 +1,24 @@
-import { UserButton } from "@clerk/nextjs";
-export default function Home() {
-  return (
-    <div>
-      <UserButton afterSignOutUrl="/sign-in" />
-      <h1>This is a protected route.</h1>
-    </div>
-  );
-}
+import { initialProfile } from "@/lib/initialProfile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+const page = async () => {
+  const profile = await initialProfile();
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
+  return <div>Create a Server</div>;
+};
+
+export default page;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import "./globals.css";
 const font = Open_Sans({ subsets: ["latin"] });
 
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+
+import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
   title: "Discord clone",
@@ -18,8 +21,18 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={font.className}>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            // forcing the dark even if the system is in light mode
+            enableSystem={false}
+            storageKey="discord-theme"
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
