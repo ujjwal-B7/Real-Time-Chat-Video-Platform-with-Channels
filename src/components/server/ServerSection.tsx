d8a929e72fe7b9ceda203b2e33c4bac1f6a19d00
ujjwal-B7 <basnetujjwal98@gmail.com>
2024-07-21 00:21:31 +0545

    Feat(api): add channel display in server sidebar,add update channel by moderator

---
+++ src/components/server/ServerSection.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { ServerWithMembersAndProfiles } from "@/types";
+import { ChannelType, MemberRole } from "@prisma/client";
+import ActionToolTip from "../ActionToolTip";
+import { Plus, Settings } from "lucide-react";
+import { useModal } from "@/hooks/useModalStore";
+
+interface ServerSectionProps {
+  label: string;
+  role?: MemberRole;
+  sectionType: "channels" | "members";
+  channelType?: ChannelType;
+  server?: ServerWithMembersAndProfiles;
+}
+
+export default function ServerSection({
+  label,
+  role,
+  sectionType,
+  channelType,
+  server,
+}: ServerSectionProps) {
+  const { onOpen } = useModal();
+  return (
+    <div className="flex items-center justify-between py-2">
+      <p
+        className="text-xs uppercase font-semibold text-zinc-500 dark:text-zinc-400
+        "
+      >
+        {label}
+      </p>
+
+      {/* Ceate channel by admin or guest  */}
+      {role !== MemberRole.GUEST && sectionType === "channels" && (
+        <ActionToolTip label="Create Channel" side="right">
+          <button
+            onClick={() => onOpen("createChannel")}
+            className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+          >
+            <Plus className="w-4 h-4" />
+          </button>
+        </ActionToolTip>
+      )}
+
+      {/* managing members by admin */}
+      {role === MemberRole.ADMIN && sectionType === "members" && (
+        <ActionToolTip label="Manage Members" side="right">
+          <button
+            onClick={() => onOpen("members", { server })}
+            className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+          >
+            <Settings className="w-4 h-4" />
+          </button>
+        </ActionToolTip>
+      )}
+    </div>
+  );
+}
--- src/components/server/ServerSideBar.tsx
+++ src/components/server/ServerSideBar.tsx
@@ -1,12 +1,32 @@
 import { currentProfile } from "@/lib/currentProfile";
 import { db } from "@/lib/db";
-import { ChannelType } from "@prisma/client";
+import { ChannelType, MemberRole } from "@prisma/client";
 import { redirect } from "next/navigation";
 import ServerHeader from "./ServerHeader";
+import { ScrollArea } from "../ui/scroll-area";
+import ServerSearch from "./ServerSearch";
+import { Hash, Mic, ShieldAlert, ShieldCheck, Video } from "lucide-react";
+import { Separator } from "../ui/separator";
+import ServerSection from "./ServerSection";
+import { ServerChannel } from "./ServerChannel";
 
 interface ServerSideBarProps {
   id: string;
 }
+
+const iconMap = {
+  [ChannelType.TEXT]: <Hash className="mr-2 h-4 w-4" />,
+  [ChannelType.AUDIO]: <Mic className="mr-2 h-4 w-4" />,
+  [ChannelType.VIDEO]: <Video className="mr-2 h-4 w-4" />,
+};
+
+const roleIconMap = {
+  [MemberRole.GUEST]: null,
+  [MemberRole.MODERATOR]: (
+    <ShieldCheck className="h-4 w-4 mr-2 text-indigo-500" />
+  ),
+  [MemberRole.ADMIN]: <ShieldAlert className="h-4 w-4 mr-2 text-rose-500" />,
+};
 
 //fetching the id and profile and details from db.server here also and in the layout also. Here is why because this side bar is also used as mobile sidebar and mobile side dont have access to the layout.tsx as in the web view component is being rendered through the layout.tsx
 const ServerSideBar = async ({ id }: ServerSideBarProps) => {
@@ -61,6 +81,69 @@ const ServerSideBar = async ({ id }: ServerSideBarProps) => {
   return (
     <div className="flex flex-col h-full text-primary w-full dark:bg-[#2b2d31] bg-[#f2f3f5]">
       <ServerHeader server={server} role={role} />
+      <ScrollArea className="flex-1 px-3">
+        <div className="mt-2">
+          <ServerSearch
+            data={[
+              {
+                label: "Text Channels",
+                type: "channel",
+                data: textChannels?.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type],
+                })),
+              },
+              {
+                label: "Voice Channels",
+                type: "channel",
+                data: audioChannels?.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type],
+                })),
+              },
+              {
+                label: "Video Channels",
+                type: "channel",
+                data: videoChannels?.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type],
+                })),
+              },
+              {
+                label: "Members",
+                type: "channel",
+                data: members?.map((member) => ({
+                  id: member.id,
+                  name: member.profile.name,
+                  icon: roleIconMap[member.role],
+                })),
+              },
+            ]}
+          />
+        </div>
+        <Separator className="bg-zinc-200 dark:bg-zinc-700 rounded-md my-2" />
+        {!!textChannels?.length && (
+          <div className="mb-2">
+            <ServerSection
+              sectionType="channels"
+              channelType={ChannelType.TEXT}
+              role={role}
+              label="Text Channels"
+            />
+            {textChannels.map((channel) => (
+              <ServerChannel
+                key={channel.id}
+                channel={channel}
+                role={role}
+                server={server}
+              />
+            ))}
+          </div>
+        )}
+      </ScrollArea>
     </div>
   );
 };
