29315f899678c9882cde773ff1ca5712934c5c88
ujjwal-B7 <basnetujjwal98@gmail.com>
2024-08-12 00:42:11 +0545

    Feat(video,audio): add video,audio call feature using livekit

--- src/app/(main)/(routes)/servers/[id]/channels/[channelId]/page.tsx
+++ src/app/(main)/(routes)/servers/[id]/channels/[channelId]/page.tsx
@@ -6,6 +6,8 @@ import { redirect } from "next/navigation";
 import { ChatHeader } from "@/components/chat/ChatHeader";
 import ChatInput from "@/components/chat/ChatInput";
 import ChatMessages from "@/components/chat/ChatMessages";
+import { ChannelType } from "@prisma/client";
+import { MediaRoom } from "@/components/MediaRoom";
 
 interface ChannelIdProps {
   params: {
@@ -45,30 +47,45 @@ export default async function ChannelPage({ params }: ChannelIdProps) {
         type="channel"
       />
 
-      <ChatMessages
-        member={member} // passing my details( logged in user details to render in the chat )
-        name={channel.name} // channel name to display #welcome to channel
-        type="channel"
-        apiUrl="/api/messages" //endpoint for loading the messages
-        socketUrl="/api/socket/messages" //endpoint for creating new messages
-        socketQuery={{
-          channelId: channel.id,
-          serverId: channel.serverId,
-        }}
-        chatId={channel.id}
-        paramValue={channel.id} //passing channel id to fetch this channels chat
-        paramKey="channelId" //passing the channels type as we are using same chat message for channel and conversation with particular
-      />
+      {/* rendering text channel */}
+      {channel.type === ChannelType.TEXT && (
+        <>
+          <ChatMessages
+            member={member} // passing my details( logged in user details to render in the chat )
+            name={channel.name} // channel name to display #welcome to channel
+            type="channel"
+            apiUrl="/api/messages" //endpoint for loading the messages
+            socketUrl="/api/socket/messages" //endpoint for creating new messages
+            socketQuery={{
+              channelId: channel.id,
+              serverId: channel.serverId,
+            }}
+            chatId={channel.id}
+            paramValue={channel.id} //passing channel id to fetch this channels chat
+            paramKey="channelId" //passing the channels type as we are using same chat message for channel and conversation with particular
+          />
 
-      <ChatInput
-        name={channel.name}
-        type="channel"
-        apiUrl="/api/socket/messages"
-        query={{
-          channelId: channel.id,
-          serverId: channel.serverId,
-        }}
-      />
+          <ChatInput
+            name={channel.name}
+            type="channel"
+            apiUrl="/api/socket/messages"
+            query={{
+              channelId: channel.id,
+              serverId: channel.serverId,
+            }}
+          />
+        </>
+      )}
+
+      {/* rendering audio channel */}
+      {channel.type === ChannelType.AUDIO && (
+        <MediaRoom chatId={channel.id} video={false} audio={true} />
+      )}
+
+      {/* rendering video channel */}
+      {channel.type === ChannelType.VIDEO && (
+        <MediaRoom chatId={channel.id} video={true} audio={false} />
+      )}
     </div>
   );
 }
